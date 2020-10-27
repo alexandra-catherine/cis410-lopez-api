795f67432bb2c91bd1b91f5338167750b97c8a51
alexandra-catherine <acl21@colostate.edu>
2020-10-27 14:21:03 -0600

    10/27 /clients/pk

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ app.get("/hi",(req,res)=>{
 
 app.get("/clients", (req,res)=>{
     //get data from db
-    db.executeQuery(`SELECT*From ClientTBL
-    Left Join WorkoutTBL
-    on ClientTBL.ClientID = WorkoutTBL.ClientID`)
+    db.executeQuery(`SELECT*From WorkoutTBL
+    Left Join ClientTBL
+    on  WorkoutTBL.ClientID = ClientTBL.ClientID `)
     .then((result)=> {
         res.status(200).send(result)
     }) 
@@ -18,6 +18,28 @@ app.get("/clients", (req,res)=>{
         res.status(500).send()
     })
 })
+app.get("/clients/:pk", (req,res)=>{
+    var pk= req.params.pk
+    //console.log("my pk: ", pk) 
+    var myQuery= `SELECT*From WorkoutTBL
+    Left Join ClientTBL
+    on  WorkoutTBL.ClientID = ClientTBL.ClientID 
+    Where ClientTBL.ClientID =  ${pk}`
 
+    db.executeQuery(myQuery)
+        .then((client) => {
+            //console.log("Client: " + client)
+            if(client[0]){
+                res.send(client[0])
+            }
+            else{
+                res.status(404).send('bad request')
+            }
+        })
+        .catch((err)=>{
+            console.log("error in clients/pk", err)
+            res.status(500).send()
+        })
+})
 app.listen(5000, ()=> 
     console.log("app is running on port 5000"))
